7b2e6263575a2c725d39449ba71e505a4fb32488
hyeongchanan <84336265+hyeongchanan@users.noreply.github.com>
2025-09-03 14:53:33 +0900

    글로벌 스타일 적용 #2
    Commonstyle 생성
    globalstyle 생성
    폰트 적용
    색상 테마 생성

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,24 +1,7 @@
 import { LoginButton } from '@/components/home_page/LoginButton';
-import styled from '@emotion/styled';
+import { Container, Text } from '@/style/CommonStyle';
 import { useNavigate } from 'react-router-dom';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: #f0f0f0;
-`;
-
-const Text = styled.div`
-  font-size: 1.25rem;
-  font-weight: 500;
-  color: #333;
-  margin-bottom: 2rem;
-  cursor: pointer;
-`;
 
 const Landing = () => {
   const navigte = useNavigate();
