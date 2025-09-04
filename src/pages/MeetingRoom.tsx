16b9d49e1da2d44c8c838c992521e07ca07d75bb
이병길 <qudrlf72@naver.com>
2025-09-04 21:30:51 +0900

    feat: 모임방 참여자 목록 UI 구현(#5)

--- src/pages/MeetingRoom.tsx
+++ src/pages/MeetingRoom.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@/components/meeting_room_page/Header';
+import { ParticipantList } from '@/components/meeting_room_page/ParticipantList';
 import { Container } from '@/style/CommonStyle';
 
 const MeetingRoom = () => {
   return (
     <Container>
       <Header />
+      <ParticipantList />
     </Container>
   );
 };
