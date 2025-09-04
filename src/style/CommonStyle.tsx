f99f5f3c350598c882c71ba8b5d28d674c306e98
이병길 <qudrlf72@naver.com>
2025-09-04 15:22:07 +0900

    chore: 공통 레이아웃 가로값 및 코드 스타일 수정

--- src/style/CommonStyle.tsx
+++ src/style/CommonStyle.tsx
@@ -1,15 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100vw;   /* 뷰포트 가로 꽉 채우기 */
+  width: 100vw; /* 뷰포트 가로 꽉 채우기 */
   height: 100vh;
-  max-width: 800px;
+  max-width: 720px;
   background-color: #ffffffff;
-;
 `;
 
 export const Text = styled.div`
