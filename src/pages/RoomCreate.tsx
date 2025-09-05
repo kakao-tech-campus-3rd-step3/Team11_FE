9515983d7bfef067c835475d36510df6149d94be
bootaeng <tgwon0947@gmail.com>
2025-09-05 15:48:56 +0900

    chore:임시저장(#8)

---
+++ src/pages/RoomCreate.tsx
@@ -0,0 +1,5 @@
+import { useEffect, useRef } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import GlobalStyle from '../style/GlobalStyle';
+import { colors } from '../style/themes';
+import apikey from '@/config/apikey';
