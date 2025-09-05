13b09e90cd5e3624044fe4392a9c38849040db73
hyeongchanan <84336265+hyeongchanan@users.noreply.github.com>
2025-09-05 14:31:54 +0900

    [FEAT] 로그인UI 구현 (#9)
    * 로그인ui구성 #7
    
    로고 이미지 추가
    스타일 추가
    
    * 랜딩 페이지 수정 #7
    
    스타일 수정

---
+++ src/pages/Landing.styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Logo = styled.img`
+  width: 80%;
+  max-width : 320px;
+  color: rgb(42, 48, 56);
+`;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,46 @@
-import { Container, Text } from '@/style/CommonStyle';
+import { Container, ContentContanier } from '@/style/CommonStyle';
+import LogoImg from '@/asset/LogoImg.png';
+import {
+  InputSection,
+  Logo,
+  LoginButton,
+  LoginMain,
+  LoginSection,
+  Spacer,
+} from './Login.styled';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
-      <Text>Login Page</Text>
+      <ContentContanier>
+      <LoginMain>
+          <Logo alt="모밋 로고" src={LogoImg} />
+          <Spacer h={24}/>
+          <LoginSection>
+            <div>
+              <InputSection
+              placeholder="이메일"
+              />
+            </div>
+            <div>
+              <InputSection
+                type="password"
+                placeholder="비밀번호"
+
+              />
+            </div>
+            <Spacer h={24}/>
+            <LoginButton onClick={() => navigate('/home')}>
+              로그인
+            </LoginButton>
+          </LoginSection>
+        </LoginMain>
+        </ContentContanier>
     </Container>
+
   );
 };
 
