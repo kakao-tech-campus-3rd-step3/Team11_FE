c2a85451fe56b250dc26484361cdcd0b570ba84e
bootaeng <tgwon0947@gmail.com>
2025-09-12 13:53:39 +0900

    feat:방 검색 페이지 UI 만들기(#17)

---
+++ src/pages/SearchRoom.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { Container } from '@/style/CommonStyle';
+
+const dummyData = [
+  {
+    id: 1,
+    title: '금정구에서 같이 공부할 분!',
+    location: '스타벅스 부산대역점',
+    category: '스터디',
+  },
+  { id: 2, title: '저녁에 농구하실 분 구합니다', location: '온천천 농구장', category: '운동' },
+  {
+    id: 3,
+    title: '주말에 보드게임 하실 분',
+    location: '히어로보드게임카페 부산대점',
+    category: '취미',
+  },
+];
+
+const SearchPageContainer = styled(Container)`
+  justify-content: flex-start;
+  height: 100vh;
+  max-width: 720px; /* Consistent max-width */
+  background-color: #ffffffff;
+`;
+
+const SearchRoom = () => {
+  return <SearchPageContainer></SearchPageContainer>;
+};
+
+export default SearchRoom;
