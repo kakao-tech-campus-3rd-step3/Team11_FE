779391e0163efb5661e1eec275f687ad194a8368
bootaeng <tgwon0947@gmail.com>
2025-09-23 13:15:45 +0900

    refactor:안쓰는 useEffect 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 import GlobalStyle from '@/style/GlobalStyle';
