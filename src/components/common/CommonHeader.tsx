5b811a565f9da8a1d198832bf8597721f226bd0e
bootaeng <tgwon0947@gmail.com>
2025-09-26 11:36:43 +0900

    refactor:공통 헤더 컴포넌트 구현

---
+++ src/components/common/CommonHeader.tsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import { colors } from '@/style/themes';
+import React from 'react';
+
+const HeaderContainer = styled.header`
+  position: absolute;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 720px;
+  height: 6rem;
+  padding: 0 2rem;
+  justify-content: flex-end;
+  align-items: center;
+  box-sizing: border-box;
+  background-color: #fff;
+  z-index: 10;
+`;
+
+const HeaderContent = styled.div`
+  position: relative;
+  width: 100%;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid ${colors.secondary300};
+`;
+
+const BackButton = styled.button`
+  all: unset;
+  cursor: pointer;
+  position: absolute;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+`;
+
+const Title = styled.h1`
+  font-size: 1.1rem;
+  font-weight: 600;
+  margin: 0;
+`;
+
+const RightContent = styled.div`
+  position: absolute;
+  right: 0;
+  display: flex;
+  align-items: center;
+  height: 100%;
+`;
+
+interface CommonHeaderProps {
+  title: string;
+  onBackButtonClick?: () => void;
+  children?: React.ReactNode;
+}
+
+export const CommonHeader = ({ title, onBackButtonClick, children }: CommonHeaderProps) => {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        {onBackButtonClick && <BackButton onClick={onBackButtonClick}>{'<'}</BackButton>}
+        <Title>{title}</Title>
+        {children && <RightContent>{children}</RightContent>}
+      </HeaderContent>
+    </HeaderContainer>
+  );
+};
