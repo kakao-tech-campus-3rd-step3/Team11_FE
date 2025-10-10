8cde94c0559061c440b046481b67bf72bfbffbd9
이병길 <qudrlf72@naver.com>
2025-10-10 12:55:47 +0900

    chore: 연결 실패 코드 (#48)

--- src/pages/MeetingRoom.tsx
+++ src/pages/MeetingRoom.tsx
@@ -3,7 +3,7 @@ import { ChatInput } from '@/components/meeting_room_page/ChatInput';
 import { Header } from '@/components/meeting_room_page/Header';
 import { mockMessages } from '@/components/meeting_room_page/mockData';
 import { ParticipantList } from '@/components/meeting_room_page/ParticipantList';
-import { useAuth } from '@/hooks/useAuth';
+// import { useAuth } from '@/hooks/useAuth';
 import { useChat } from '@/hooks/useChat';
 import { Container } from '@/style/CommonStyle';
 import type { ChatMessage } from '@/types/meeting_room_page/chatMessage';
@@ -13,11 +13,11 @@ import { useEffect, useState } from 'react';
 const MeetingRoom = () => {
   const [chatMessages, setChatMessages] = useState<ChatMessage[]>(mockMessages);
   const { connect } = useChat('95ee73b5-9c1c-4224-b445-59094dc20152');
-  const { login } = useAuth();
+  // const { login } = useAuth();
 
   useEffect(() => {
     (async () => {
-      login({ email: 'user@test.com', password: 'testpass1212!' });
+      // login({ email: 'user@test.com', password: 'testpass1212!' });
       console.log(`Token: ${getAccessToken()}`);
       connect();
     })();
