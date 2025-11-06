eb109166c47ac073509df6dd956fd7f7f33117bc
hyeongchanan <84336265+hyeongchanan@users.noreply.github.com>
2025-11-07 00:05:06 +0900

    Update CommonStyle.tsx

--- src/style/CommonStyle.tsx
+++ src/style/CommonStyle.tsx
@@ -12,21 +12,6 @@ export const Container = styled.div`
   margin: 0 auto;
   background-color: #ffffffff;
   overflow: hidden;
-
-  /* Tablet */
-  @media (min-width: 768px) {
-    max-width: 960px;
-  }
-
-  /* Small Desktop */
-  @media (min-width: 1024px) {
-    max-width: 1200px;
-  }
-
-  /* Large Desktop */
-  @media (min-width: 1440px) {
-    max-width: 1320px;
-  }
 `;
 
 export const ContentContanier = styled.div`
